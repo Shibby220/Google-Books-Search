18b0af3dbf1da03f2adbcf77ae025b05d1a96ebc
Daniel Spira <daniel.b.spira@gmail.com>
2019-11-02 12:35:42 -0700

    cleaned up routes

--- routes/api/book.js
+++ routes/api/book.js
@@ -1,14 +1,11 @@
-// import express Router and the book controller
 const router = require("express").Router();
 const bookController = require("../../controllers/book");
 
-// Matches with "/api/book"
 router
   .route("/")
   .get(bookController.findAll)
   .post(bookController.create);
 
-// Matches with "/api/book/:id"
 router
   .route("/:id")
   .get(bookController.findById)
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,6 @@
-// import api routes and express router
 const router = require("express").Router();
 const bookRoutes = require("./book");
 
-// prefix book route endpoint with "/book"
 router.use("/book", bookRoutes);
 
-// export routes
 module.exports = router;
